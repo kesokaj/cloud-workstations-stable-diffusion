7aaebb89933280a3154d2386e6862b4e25961948
kesokaj <ostling.simon@gmail.com>
2024-04-09 11:20:43 +0200

    niong2ahbaixeegh8poovieGhahwakei

--- 200_start-webui.sh
+++ 200_start-webui.sh
@@ -6,7 +6,6 @@ cuda_status=$(python3 -c 'import torch; print(torch.cuda.is_available())')
 
 if [[ $cuda_status == "True" ]]; then
     nvidia-smi
-    nvcc -V
     runuser -l user -c ./webui.sh --install_dir="/home/$USER" --xformers --listen --port ${EXPOSE_PORT} &
 else
     runuser -l user -c ./webui.sh --install_dir="/home/$USER" --skip-torch-cuda-test --precision full --no-half --listen --port ${EXPOSE_PORT} &
