09d886073192d16007cd6982d2de69a61d0cfc28
kesokaj <ostling.simon@gmail.com>
2024-04-09 11:32:27 +0000

    Bye!behalf9Flip

--- 200_start-webui.sh
+++ 200_start-webui.sh
@@ -2,13 +2,16 @@
 
 set -eoux pipefail
 
-cuda_status=$(python3 -c 'import torch; print(torch.cuda.is_available())')
+if [ ! -f webui.sh ]; then 
+    runuser -l user -c 'wget -q https://raw.githubusercontent.com/AUTOMATIC1111/stable-diffusion-webui/master/webui.sh'
+    runuser -l user -c 'chmod a+x webui.sh'
+fi
 
-if [[ $cuda_status == "True" ]]; then
+if command -v nvidia-smi >/dev/null 2>&1; then
     nvidia-smi
-    runuser -l user -c ./webui.sh --install_dir="/home/$(whoami)" --xformers --listen --port ${EXPOSE_PORT} &
+    runuser -l user -c './webui.sh --xformers --listen --port 80 &'
 else
-    runuser -l user -c ./webui.sh --install_dir="/home/$(whoami)" --skip-torch-cuda-test --precision full --no-half --listen --port ${EXPOSE_PORT} &
+    runuser -l user -c '/webui.sh --skip-torch-cuda-test --precision full --no-half --listen --port 80 &'
 fi
 
 exit 0
--- Dockerfile
+++ Dockerfile
@@ -2,7 +2,6 @@ FROM us-central1-docker.pkg.dev/cloud-workstations-images/predefined/base:latest
 
 LABEL org.opencontainers.image.source="https://github.com/kesokaj/stable-diffusion-ui-dockerfile"
 
-ENV EXPOSE_PORT="80"
 ENV DEBIAN_FRONTEND="noninteractive"
 
 RUN apt-get update && apt-get install -y --no-install-recommends \
@@ -14,12 +13,10 @@ RUN apt-get update && apt-get install -y --no-install-recommends \
     python3-pip \
     python-is-python3
 
-RUN wget -q https://raw.githubusercontent.com/AUTOMATIC1111/stable-diffusion-webui/master/webui.sh
-RUN chmod a+x webui.sh
-RUN pip3 install torch
+RUN pip3 install --upgrade pip
 
 COPY 120_start-gcsfuse.sh /etc/workstation-startup.d/
 COPY 200_start-webui.sh /etc/workstation-startup.d/
 RUN chmod a+x /etc/workstation-startup.d/*
 
-EXPOSE ${EXPOSE_PORT}
+EXPOSE 80
